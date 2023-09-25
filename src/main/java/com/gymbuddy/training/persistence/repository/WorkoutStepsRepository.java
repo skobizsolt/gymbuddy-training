7b79d2f0688b7372c6ae5fe59a80367f3f24c811
Erik Zsolt Skobrák <skobizsolt@gmail.com>
2023-09-25 20:01:21 +0200

    fix: GYM-15 unused jpa repository query deleted

--- src/main/java/com/gymbuddy/training/persistence/repository/WorkoutStepsRepository.java
+++ src/main/java/com/gymbuddy/training/persistence/repository/WorkoutStepsRepository.java
@@ -1,7 +1,6 @@
 package com.gymbuddy.training.persistence.repository;
 
 import com.gymbuddy.training.persistence.domain.WorkoutStep;
-import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -27,10 +26,4 @@ public interface WorkoutStepsRepository extends JpaRepository<WorkoutStep, Long>
     @Query(value = "SELECT MAX(step_number) + 1 FROM workout_step s" +
             " WHERE s.workout_id = :workoutId", nativeQuery = true)
     Optional<Integer> getNextStepNumber(Long workoutId);
-
-    @Query(value = "SELECT COUNT(workout_id) as totalSteps," +
-            " SUM(estimated_time) as estimatedTimeInMinutes" +
-            " FROM workout_step s" +
-            " WHERE s.workout_id = :workoutId", nativeQuery = true)
-    Tuple getGeneralDataByWorkoutId(Long workoutId);
 }
